c45b1f279cacffd775f4b5c652f743f82ac0568d
safina3d <safina3d@users.noreply.github.com>
2023-12-05 10:03:12 +0100

    update import react

--- dist/DatePicker/IconButton/IconButton.js
+++ dist/DatePicker/IconButton/IconButton.js
@@ -1,4 +1,4 @@
-"use strict";Object.defineProperty(exports,"__esModule",{value:true});exports.default=IconButton;require("./IconButton.css");var _reactFontawesome=require("@fortawesome/react-fontawesome");/**
+"use strict";Object.defineProperty(exports,"__esModule",{value:true});exports.default=IconButton;require("./IconButton.css");var _react=_interopRequireDefault(require("react"));var _reactFontawesome=require("@fortawesome/react-fontawesome");function _interopRequireDefault(obj){return obj&&obj.__esModule?obj:{default:obj}}/**
  * IconButton component for displaying an icon button.
  * @module IconButton
  * @param {Object} props - Component properties.
@@ -6,4 +6,4 @@
  * @param {function} props.onClick - Click event handler for the button.
  * @returns {JSX.Element} The IconButton component.
  */function IconButton(_ref){let{icon,onClick}=_ref;// Handles the click event on the IconButton.
-const handleClick=event=>{onClick(event)};return/*#__PURE__*/React.createElement("div",{onClick:handleClick,className:"IconButton"},/*#__PURE__*/React.createElement(_reactFontawesome.FontAwesomeIcon,{icon:icon,className:"IconButton__button"}))}
+const handleClick=event=>{onClick(event)};return/*#__PURE__*/_react.default.createElement("div",{onClick:handleClick,className:"IconButton"},/*#__PURE__*/_react.default.createElement(_reactFontawesome.FontAwesomeIcon,{icon:icon,className:"IconButton__button"}))}
